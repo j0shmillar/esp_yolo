35f8ae7bb1cc69abc3262d5d20ed13d122ac65d5
daleonpz <daleonpz@gmail.com>
2024-01-11 17:53:30 +0100

    increase stack task

--- application/main/main.cc
+++ application/main/main.cc
@@ -38,6 +38,6 @@ void tf_main(void) {
 }
 
 extern "C" void app_main() {
-  xTaskCreate((TaskFunction_t)&tf_main, "tf_main", 4 * 1024, NULL, 8, NULL);
+  xTaskCreate((TaskFunction_t)&tf_main, "tf_main", 10 * 1024, NULL, 8, NULL);
   vTaskDelete(NULL);
 }
